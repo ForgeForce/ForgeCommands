e65635e880d9e206f178408adceaad5a933b8dd9
Matyrobbrt <matyrobbrt@gmail.com>
2022-12-20 17:42:15 +0200

    Fix

--- src/main/java/net/minecraftforge/actionable/Main.java
+++ src/main/java/net/minecraftforge/actionable/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Args: " + Arrays.toString(args));
         System.getenv().forEach((key, val) -> {
             System.out.println("Key: " + key + ", Val: " + val);
         });
