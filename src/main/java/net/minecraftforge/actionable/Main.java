9efe257babee0a4756b1524c3d25dbef81aa393c
Matyrobbrt <matyrobbrt@gmail.com>
2022-12-20 19:30:08 +0200

    Fix

--- src/main/java/net/minecraftforge/actionable/Main.java
+++ src/main/java/net/minecraftforge/actionable/Main.java
@@ -1,18 +1,41 @@
 package net.minecraftforge.actionable;
 
-import java.io.IOException;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.DERNull;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.kohsuke.github.GitHubBuilder;
+import org.kohsuke.github.extras.authorization.JWTTokenProvider;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.Base64;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println(Files.readString(Path.of(System.getenv("GITHUB_EVENT_PATH"))));;
+    public static void main(String[] args) throws Exception {
+        System.out.println(Files.readString(Path.of(System.getenv("GITHUB_EVENT_PATH"))));
+
         System.out.println("\n");
 
-        System.out.println("Args: " + Arrays.toString(args));
-        System.getenv().forEach((key, val) -> {
-            System.out.println("Key: " + key + ", Val: " + val);
-        });
+        byte[] pkcs1Encoded = Base64.getDecoder().decode(args[1]);
+
+        AlgorithmIdentifier algId = new AlgorithmIdentifier(PKCSObjectIdentifiers.rsaEncryption, DERNull.INSTANCE);
+        PrivateKeyInfo privateKeyInfo = new PrivateKeyInfo(algId, ASN1Sequence.getInstance(pkcs1Encoded));
+
+        byte[] pkcs8Encoded = privateKeyInfo.getEncoded();
+
+        final String key = (String) JWTTokenProvider.class.getDeclaredMethod("refreshJWT").invoke(new JWTTokenProvider(args[0], Base64.getEncoder().encodeToString(pkcs8Encoded)));
+
+        final var team = new GitHubBuilder()
+                .withJwtToken(key)
+                .build()
+                .getOrganization("ForgeForce")
+                .getTeamByName("triagers");
+
+        System.out.println("So, the team members are: ");
+        for (final var member : team.getMembers()) {
+            System.out.println(member.getName());
+        }
     }
 }
