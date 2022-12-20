a7a4fb70b99ec350ef1036aebbf5aac7454ffd17
Matyrobbrt <matyrobbrt@gmail.com>
2022-12-20 20:05:55 +0200

    Fix

--- src/main/java/net/minecraftforge/actionable/Main.java
+++ src/main/java/net/minecraftforge/actionable/Main.java
@@ -1,5 +1,8 @@
 package net.minecraftforge.actionable;
 
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.DERNull;
 import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
@@ -13,14 +16,16 @@ import java.lang.invoke.MethodType;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.KeyFactory;
+import java.security.PrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Base64;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws Throwable {
         // System.out.println(Files.readString(Path.of(System.getenv("GITHUB_EVENT_PATH"))));
-
-        final var lookup = (MethodHandles.Lookup)MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP").get(null);
 
         byte[] pkcs1Encoded = Base64.getDecoder().decode(args[1].replaceAll("\\s", ""));
 
@@ -29,8 +34,7 @@ public class Main {
 
         byte[] pkcs8Encoded = privateKeyInfo.getEncoded();
 
-        final String key = (String) lookup.findSpecial(JWTTokenProvider.class, "refreshJWT", MethodType.methodType(String.class), JWTTokenProvider.class)
-                .invoke(new JWTTokenProvider(args[0], KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(pkcs8Encoded))));
+        final String key = refreshJWT(args[0], KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(pkcs8Encoded)));
 
         final var team = new GitHubBuilder()
                 .withJwtToken(key)
@@ -42,5 +46,30 @@ public class Main {
         for (final var member : team.getMembers()) {
             System.out.println(member.getName());
         }
+    }
+
+    private static String refreshJWT(String appId, PrivateKey privateKey) {
+        Instant now = Instant.now();
+
+        // Max token expiration is 10 minutes for GitHub
+        // We use a smaller window since we likely will not need more than a few seconds
+        Instant expiration = now.plus(Duration.ofMinutes(9));
+
+        // Setting the issued at to a time in the past to allow for clock skew
+        Instant issuedAt = getIssuedAt(now);
+
+        // Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder()
+                .setIssuedAt(Date.from(issuedAt))
+                .setExpiration(Date.from(expiration))
+                .setIssuer(appId)
+                .signWith(privateKey, SignatureAlgorithm.RS256);
+
+        // Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
+
+    static Instant getIssuedAt(Instant now) {
+        return now.minus(Duration.ofMinutes(2));
     }
 }
