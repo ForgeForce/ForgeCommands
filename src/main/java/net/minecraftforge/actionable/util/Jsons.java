adab0df72de5eb101e98b141bea24c00c077a40d
Matyrobbrt <matyrobbrt@gmail.com>
2022-12-21 18:44:43 +0200

    Fix

--- src/main/java/net/minecraftforge/actionable/util/Jsons.java
+++ src/main/java/net/minecraftforge/actionable/util/Jsons.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 public class Jsons {
     public static JsonNode at(JsonNode node, String path) {
         final String[] paths = path.split("\\.");
-        for (int i = path.length() - 1; i >= 0; i--) {
+        for (int i = paths.length - 1; i >= 0; i--) {
             node = node.get(paths[i]);
         }
         return node;
