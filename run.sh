416a054b2c2f3cba975292253dcd79eeb0479949
Matyrobbrt <matyrobbrt@gmail.com>
2022-12-22 11:59:29 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java --add-opens java.base/java.net=ALL-UNNAMED -jar ../../app.jar
+java --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.base/sun.net.www.protocol.https=ALL-UNNAMED -jar ../../app.jar
